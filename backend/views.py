62682b619f1f2c03fcabf3085045d3dfb15f5c5c
johny <johny.tran2021@gmail.com>
2024-05-22 00:55:07 -0400

    Added page to showcase in development games and a basic test game. This came with some caveates, namely that it is now possible to directly search for the game files.

--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,17 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash, current_app
+from flask import Blueprint, render_template, request, redirect, url_for, flash, current_app, send_from_directory
 from .models import * 
 from . import db
+import mimetypes
 
 views = Blueprint('views', __name__)
+
+mimetypes.add_type('application/javascript', '.js')
+
+@views.after_request
+def set_headers(response):
+    response.headers['Cross-Origin-Opener-Policy'] = 'same-origin'
+    response.headers['Cross-Origin-Embedder-Policy'] = 'require-corp'
+    return response
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
@@ -11,6 +20,19 @@ def home():
 @views.route('/resume', methods=['GET', 'POST'])
 def resume():
     return render_template("resume.html")
+
+@views.route('/game', methods=['GET', 'POST'])
+def game():
+    return render_template("game.html")
+
+@views.route('/display', methods=['GET', 'POST'])
+def display():
+   return send_from_directory("static/game", "test.html")
+
+@views.route('/<path:path>')
+def static_files(path):
+    print("Attempting access to " + path)
+    return send_from_directory('static/game', path)
 
 @views.route('/comment', methods=['GET', 'POST'])
 def comment():
